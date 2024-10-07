1c17976c88313c720561e2a9ff069688d0bb6ab8
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-07 23:27:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ArrSum
+{
+    public class ArraySum
+    {
+
+        static void Main(string[] args)            //main method
+        {
+
+            Console.WriteLine("enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("enter array elements:");
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) //taking array elements
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("array sum is:" + sum);   //sum of array elements
+
+        }
+    }
+}
